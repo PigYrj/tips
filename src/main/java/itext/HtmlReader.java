e56653cbf7f8403c4bc245668d19a65533c49927
yurj@mail.unitid.cn <www.484140554@qq.com>
2019-03-05 15:32:22 +0800

    html转pdf

---
+++ src/main/java/itext/HtmlReader.java
@@ -0,0 +1,56 @@
+package itext;
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.io.font.FontProgram;
+import com.itextpdf.io.font.FontProgramFactory;
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.layout.font.FontProvider;
+import com.itextpdf.layout.font.FontSet;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 利用HTML代码片段生成PDF
+ *
+ * zkh
+ * 2018年3月13日 下午2:05:17
+ */
+public class HtmlReader {
+
+	public static void toPdf(String html, String DEST) throws IOException {
+		/*
+		 * 常规属性设置
+		 */
+		ConverterProperties props = new ConverterProperties();
+        //字体方案
+        FontProgram fontProgram = FontProgramFactory.createFont();
+        //转换为pfd中的属性设置对象
+        ConverterProperties proper = new ConverterProperties();
+        //字体设置，解决中文不显示问题
+        FontSet fontSet = new FontSet();
+        fontSet.addFont(fontProgram, PdfEncodings.IDENTITY_H);
+        FontProvider fontProvider = new FontProvider(fontSet);
+        proper.setFontProvider(fontProvider);
+
+        HtmlConverter.convertToPdf(html, new FileOutputStream(DEST), props);
+
+	}
+
+	public static void main(String[] args) throws Exception{
+		//文件生成路径
+		String dest = "C:\\Users\\YRJ\\Desktop\\pd1\\gen2.pdf";
+
+		//抓取的网页地址
+		String path = "https://en.wikipedia.org/wiki/KJHM";
+		Document htmlContent = Jsoup.connect(path).get();
+		try {
+			toPdf(htmlContent.html(), dest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
